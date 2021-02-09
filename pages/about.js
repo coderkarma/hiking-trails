ca1e16990a89081bd75615056154c9e32ecb60bf
Karma Drukpa <karmadrukpa11@gmail.com>
2021-02-08 23:34:50 -0800

    install bootstrap

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
 
 const AboutPage = () => {
-    return <h1>This is about page</h1>;
+    return (
+        <div>
+            <h1 className="d-flex justify-content-center">
+                This is about page
+            </h1>
+            ;<Button>Bootstrap + Nextjs</Button>
+        </div>
+    );
 };
 
 export default AboutPage;
